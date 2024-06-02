f4afafcb02654128e1a5834df156ab33c49977a5
rizk <rizkdotai@gmail.com>
2024-06-02 13:28:54 +0300

    handle events in response stream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+from typing_extensions import override
+from openai import AssistantEventHandler
 
 load_dotenv()
 
@@ -24,12 +26,48 @@ message = client.beta.threads.messages.create(
     content="How do I get started working out to lose fat and build muscles?"
 )
 
-run = client.bete.threads.run.create_and_poll(
-    thread_id=thread.id,
-    assistant_id=personal_trainer.id,
-    instructions = "Please address the user as Rizk."
 
-)
+
+
+# First, we create a EventHandler class to define
+# how we want to handle the events in the response stream.
+ 
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+  
+  def on_tool_call_delta(self, delta, snapshot):
+    if delta.type == 'code_interpreter':
+      if delta.code_interpreter.input:
+        print(delta.code_interpreter.input, end="", flush=True)
+      if delta.code_interpreter.outputs:
+        print(f"\n\noutput >", flush=True)
+        for output in delta.code_interpreter.outputs:
+          if output.type == "logs":
+            print(f"\n{output.logs}", flush=True)
+ 
+# Then, we use the `stream` SDK helper 
+# with the `EventHandler` class to create the Run 
+# and stream the response.
+ 
+with client.beta.threads.runs.stream(
+  thread_id=thread.id,
+  assistant_id=personal_trainer.id,
+  instructions="Please address the user as Rizk.",
+  event_handler=EventHandler(),
+) as stream:
+  stream.until_done()
+
+
+
 
 
 
